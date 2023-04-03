a53add5665cd956017dbc18cbd7a0fc4c719a050
Tomas Scerbak <tomas.scerbak@gmail.com>
2023-04-03 14:46:11 +0200

    fixing https issue

--- src/Services/APIs/WorldTimeAPI.jsx
+++ src/Services/APIs/WorldTimeAPI.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const worldTimeAPI = "http://worldtimeapi.org/api/ip";
+const worldTimeAPI = "https://worldtimeapi.org/api/ip";
 
 export const WorldTimeAPI = async () => {
   const { data } = await axios.get(worldTimeAPI);
